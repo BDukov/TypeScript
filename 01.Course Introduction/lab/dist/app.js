06ce208b5c82202fa823b5ea0d992aea5ffd3d19
Borislav Dukov <borislav.dukov@icloud.com>
2023-10-29 09:04:25 +0200

    OOP TypeScript

---
+++ 01.Course Introduction/lab/dist/app.js
@@ -0,0 +1,88 @@
+"use strict";
+// 1. Hello World
+const text = "Hellow World";
+console.log(text);
+console.log(`---------------------------------------------------------`);
+// 2. Numbers 1 to 10
+let output;
+for (let index = 1; index <= 10; ++index) {
+    output = index;
+    console.log(output);
+}
+console.log(`---------------------------------------------------------`);
+// 3.Calculate Rectangle Area
+function calcRectangleArea(a, b) {
+    let area = a * b;
+    console.log(area);
+}
+calcRectangleArea(5, 7);
+calcRectangleArea(6, 8);
+console.log(`---------------------------------------------------------`);
+// 4. Convertor Inches to Centimeters
+function inchesToCentimeters(inches) {
+    let centimeters = inches * 2.54;
+    console.log(centimeters);
+}
+inchesToCentimeters(5);
+inchesToCentimeters(7);
+console.log(`---------------------------------------------------------`);
+// 5. Calculate Circle Area
+function calcCircleArea(radius) {
+    let area = Math.PI * radius * radius;
+    console.log(area.toFixed(2));
+}
+calcCircleArea(5);
+console.log(`---------------------------------------------------------`);
+// 6. String Length
+function stringLength(str) {
+    console.log(str.length);
+}
+stringLength("chocolate");
+stringLength("pasta");
+console.log(`---------------------------------------------------------`);
+// 7. Largest Number
+function largestNumber(a, b, c) {
+    console.log(Math.max(a, b, c));
+}
+largestNumber(5, -3, 16);
+largestNumber(-3, -5, -22.5);
+console.log(`---------------------------------------------------------`);
+// 8. Sum of Numbers N..M
+function sumAllNumbers(n, m) {
+    let num1 = Number(n);
+    let num2 = Number(m);
+    let sum = 0;
+    for (let index = num1; index <= num2; index++) {
+        sum = sum + index;
+    }
+    console.log(sum);
+}
+sumAllNumbers("1", "5");
+sumAllNumbers("-8", "20");
+console.log(`---------------------------------------------------------`);
+// 9. Day of Week
+function dayOfWeek(day) {
+    switch (day) {
+        case "Monday":
+            console.log(1);
+            break;
+        case "Tuesday":
+            console.log(2);
+            break;
+        case "Wednesday":
+            console.log(3);
+            break;
+        case "Thursday":
+            console.log(4);
+            break;
+        case "Friday":
+            console.log(5);
+            break;
+        default:
+            console.log("Invalid");
+            break;
+    }
+}
+dayOfWeek("Monday");
+dayOfWeek("Friday");
+//# sourceMappingURL=app.js.map
